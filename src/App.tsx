9516aaa50e79243996f583a4fd37da07a889ee9e
Nikola Horvat <the.final.frontend@gmail.com>
2020-09-23 12:01:38 +0200

    chore: add content Running records

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "./logo.svg";
+//import logo from "./logo.svg";
 import "./App.css";
 import "antd/dist/antd.css";
 
@@ -7,10 +7,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <span>Running records</span>
+        <ul style={{ listStyle: "none", fontSize: "0.7em" }}>
+          <li>Marathon time: 2:44:37 (Zagreb 2016) </li>
+          <li>24h: 211.600km (Brussels Circadian Race 2018)</li>
+        </ul>
       </header>
     </div>
   );
 }
 
 export default App;
+
+//https://www.uitagendautrecht.nl/en/nikola-horvat/
